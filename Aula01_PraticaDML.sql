fee3058668cd21516fdfa240f4c6cc34abbeb0cc
Rafael Macedo <rafael9408@gmail.com>
2022-06-22 22:07:10 -0300

    Prática feita com exemplo do código para fixação de comandos no SQLite

---
+++ Aula01_PraticaDML.sql
@@ -0,0 +1,12 @@
+-- Prática com exemplo do Projeto 1.0 para exercitar comandos SELECT, INSERT E DELETE
+
+select * from ClientesPF; 
+
+insert into ClientesPF(nome,CPF)values("Maya Yasmin Analu Fogaça", "611.770.469-03");
+insert into ClientesPF(nome,CPF)values("Nelson Gael Filipe da Silva", "696.481.582-15");
+insert into ClientesPF(nome,CPF)values("Severino Elias Kevin Teixeira", "604.550.601-42");
+insert into ClientesPF(nome,CPF)values("Enrico Carlos Marcos Rezende", "626.710.312-97");
+insert into ClientesPF(nome,CPF)values("Isadora Luana da Silva", "224.065.248-92");
+
+
+delete from ClientesPF;
